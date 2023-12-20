80c6aadcee78202fd82b8a492dbea27aa1ddcdf1
Andika Alakate <andikaalakate@gmail.com>
2023-12-20 22:34:59 +0700

    Video Part 9

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins']
-      }
+        poppins: ['Poppins'],
+        firaMono: ['Fira Mono'],
+        firaSans: ['Fira Sans'],
+        firaCode: ['Fira Code']
+      },
+      screens: {
+        'hp': '576px',
+        'mini': '320px'
+      },
     },
   },
   plugins: [],
